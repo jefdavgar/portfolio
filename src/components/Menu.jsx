3964c88d5c301e3621481953b768c42390a044d8
Jeferson Camero <jeferson.camero@cun.edu.co>
2022-12-16 18:59:51 -0500

    Deleted the default code of app.jsx and app.css. A folder called components was created that contains a component called menu where the menu structure made with Nav, Navbar and Offcanvas is found. In addition, the Bootstrap library was installed, which has default ta styles (Imported in the main.jsx). In the components folder you will find the menu.css file that corresponds to the menu.jsx styles

---
+++ src/components/Menu.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+
+
+function Example() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      {["md"].map((expand) => (
+        <Navbar
+          key={expand}
+          expand={expand}
+          className="mb-3"
+          id="NavBar-nav"
+          sticky="top"
+        >
+          <Container>
+            <Navbar.Brand href="#"></Navbar.Brand>
+            <Navbar.Toggle
+              aria-controls={`offcanvasNavbar-expand-${expand}`}
+            ></Navbar.Toggle>
+            <Navbar.Offcanvas
+              className="offcanvas-navbar"
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Body show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Nav>
+                    <Nav.Link href="#Inicio">HOME</Nav.Link>
+                    <Nav.Link href="#Inicio">SOBRE MI</Nav.Link>
+                    <Nav.Link href="#Inicio">PROYECTOS</Nav.Link>
+                    <Nav.Link href="#Inicio">CONTACTO</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+export default Example;
